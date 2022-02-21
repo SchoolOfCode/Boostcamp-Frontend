2230b278c2b9d16d9d6b90a6c6b262eb89f62dcf
Richard Mckenzie <richardmckenzie93@gmail.com>
2022-02-21 11:48:46 +0000

    updated readme with typescript task and created onscreen short medium and long boilerplate pages

--- README.md
+++ README.md
@@ -11,3 +11,5 @@ Implementation
 - Create avatar
 - Import images for task type
 - Check view on multiple screens - change all units to relative if needed
+- Typing of all components
+- Testing
---
+++ boostcamp/components/OnScreenShort.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+export default function OnScreenShort({ navigation }: any) {
+  // const onPressHandler = () => {
+  //   navigation.goBack();
+  // };
+
+  return (
+    <View>
+      <Text>OnScreen Short Tasks Screen</Text>
+    </View>
+  );
+}
