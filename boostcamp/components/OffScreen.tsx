a94fc5be2388cc6ab91a9fb25ddb93fc9c4bc44e
Richard Mckenzie <richardmckenzie93@gmail.com>
2022-02-21 11:36:03 +0000

    set up a few screens and began building adams initial high fidelity wireframes

---
+++ boostcamp/components/OffScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+export default function OffScreen({ navigation }: any) {
+  // const onPressHandler = () => {
+  //   navigation.goBack();
+  // };
+
+  return (
+    <View>
+      <Text>Offscreen Tasks Screen</Text>
+    </View>
+  );
+}
