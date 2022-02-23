05a80b98f2fb08bb98eb0a286d725e06b52473d7
Richard Mckenzie <richardmckenzie93@gmail.com>
2022-02-23 08:41:20 +0000

    added random task functionality

--- boostcamp/components/OffScreenShortScreen.tsx
+++ boostcamp/components/OffScreenShortScreen.tsx
@@ -19,7 +19,7 @@ export default function OffScreenShortScreen({ navigation }: any) {
     async function getTask() {
       const response = await fetch(`${API_URL}tasks/offscreen/short`);
       const data = await response.json();
-      setTask(data.payload[0]);
+      setTask(data.payload[Math.random() * data.length]);
     }
     getTask();
   }, []);
