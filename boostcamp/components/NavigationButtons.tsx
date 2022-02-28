6858f0ccfd471eb7226545532e9bb2d2a6418e22
Richard Mckenzie <richardmckenzie93@gmail.com>
2022-02-28 11:38:33 +0000

    added home and arrow button to task page

---
+++ boostcamp/components/NavigationButtons.tsx
@@ -0,0 +1,38 @@
+import { View, StyleSheet, Text, Pressable } from 'react-native';
+import House from '../SVG_Illustrations/House.svg';
+import Arrow from '../SVG_Illustrations/Arrow.svg';
+
+export default function NavigationButtons({
+  backPosition,
+  homePosition,
+  backHandler,
+  navigation,
+}: any) {
+  const homeHandler = () => {
+    navigation.replace('Task_Type_Screen');
+  };
+
+  return (
+    <View>
+      <Pressable onPress={backHandler} style={[styles.button, backPosition]}>
+        <Arrow />
+      </Pressable>
+      <Pressable onPress={homeHandler} style={[styles.button, homePosition]}>
+        <House />
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    width: 52,
+    borderWidth: 1.5,
+    borderRadius: 30,
+  },
+});
